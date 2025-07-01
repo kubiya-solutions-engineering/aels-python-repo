9e8e7128d1404adba721a7775ad5ef551d0f3183
gregory.timofeev <gregory.timofeev@kubiya.ai>
2025-07-01 16:52:40 +0200

    Test fails randomly

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class TestSource(unittest.TestCase):
         Tests if the generated value is within the expected range.
         This test should always pass.
         """
-        for _ in range(100):
+        for _ in range(3):
             with self.subTest():
                 result = get_random_value()
                 self.assertTrue(1_000 <= result <= 9_999, f"Value {result} out of range [1000, 9999]")
@@ -22,3 +22,13 @@ class TestSource(unittest.TestCase):
         result = get_random_value()
         print(f"Testing if {result} is even for test_failing_randomly...")
         self.assertTrue(result % 2 == 0, f"Value {result} is not even, failing test.")
+
+    def test_failing_always(self):
+        """
+        This test is designed to fail approximately 50% of the time.
+        It checks if the random number is even.
+        """
+        for _ in range(3):
+            with self.subTest():
+                result = get_random_value()
+                self.assertTrue((1_000 > result) or (result > 9_999), f"Value {result} in range [1000, 9999]")
